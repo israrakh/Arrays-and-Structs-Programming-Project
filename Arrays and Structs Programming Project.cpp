ccdde7d0eefb7ec9b5ba204a04a6de13871b67e4
Israr <israr.akh03@gmail.com>
2024-10-16 19:10:17 -0400

    Add project files.

---
+++ Arrays and Structs Programming Project.cpp
@@ -0,0 +1,10 @@
+// Arrays and Structs Programming Project.cpp : This file contains the Marathon Runners with Parallel Arrays and Structs.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This file contains the Marathon Runners with Parallel Arrays and Structs." << endl;
+}
